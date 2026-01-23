0f1b87d930d1a62d9bce8daf9587b67e6a7c7679
Victor Castro <victorcaucastro@gmail.com>
2026-01-23 11:34:12 -0300

    Validating user data

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import { z } from "zod"
 
 class UsersController {
-  create(request: Request, response: Response) {
+  create(request: Request, response: Response) {   // controller para retornar o conteúdo da requisição.
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),                   // validação do que é necessário para cadastrar usuário.
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
     return response.json({ message: "ok" })
   }
 }
