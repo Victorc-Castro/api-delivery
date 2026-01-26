71ae01724f677c80f68c37d5f73c8b0f2b630de6
Victor Castro <victorcaucastro@gmail.com>
2026-01-25 21:53:39 -0300

    Registering log

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,37 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"  // importação do erro para verificação.
+import { prisma } from "@/database/prisma"        // importação do banco de dados.
+import { z } from "zod"                        // importação do zod para validações.
 
-class DeliveryLogsController {  // método para adicionar informações sobre os envios.
+class DeliveryLogsController {                             // método para adicionar informações sobre os envios.
   async create(resquest: Request, response: Response) {
-    return response.json({ message: "ok" })
+    const bodySchema = z.object({  
+      delivery_id: z.string().uuid(),                     // validação do que é necessário para o log.
+      description: z.string()
+    })
+
+    const { delivery_id, description } = bodySchema.parse(resquest.body)
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id }                         // buscando o id no banco de dados.
+    })
+
+    if(!delivery){
+      throw new AppError("delivery not found", 404)     // validação de se o pedido existe.
+    }
+
+    if(delivery.status === "processing"){
+      throw new AppError("change status to shipped")   // validação de se o status ainda está como "procesing", pois o pedido precisa ser enviado para o log.
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveredId: delivery_id,                     // criando o método para o log.
+        description
+      }
+    })
+
+    return response.status(201).json()
   }
 }
 
