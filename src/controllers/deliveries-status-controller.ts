dee802ee152d12557c5112fbeab26d78a48023d4
Victor Castro <victorcaucastro@gmail.com>
2026-01-25 21:01:30 -0300

    Updating delivery status

---
+++ src/controllers/deliveries-status-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express'
+import { prisma } from '@/database/prisma'  // importação do banco de dados.
+import { z } from "zod"  // zod para as validações.
+
+class DeliveriesStatusController {
+  async update(request: Request, reponse: Response) {  // método para atualizar os status.
+    const paramsSchema = z.object({
+      id: z.string().uuid(),                     // validações dos parâmetros.
+    })
+
+    const bodySchema = z.object({
+      status: z.enum(["processing", "shipped", "delivered"]),   // validação do corpo da requisição.
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+    const { status } = bodySchema.parse(request.body)
+
+    await prisma.delivery.update({
+      data: {
+        status,
+      },
+      where: {                             // usando o prisma para dizer o que queremos atualizar.
+        id,
+      },
+    })
+
+    return reponse.json()
+  }
+}
+
+export { DeliveriesStatusController }
--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express"
 
 import { DeliveriesController } from "@/controllers/deliveries-controller"
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller"  // importando o controller de atualização.
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated" // importação da autenticação que criei. 
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization" // importação da função de verificação na rota.
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController() // instânciando a classe de atualização de status.
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))  // passando a autenticação para a rota.
 deliveriesRoutes.post("/", deliveriesController.create)
 deliveriesRoutes.get("/", deliveriesController.index) // rota para listar pedidos.
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update)  // rota para atualizar o status.
 
 export { deliveriesRoutes }
