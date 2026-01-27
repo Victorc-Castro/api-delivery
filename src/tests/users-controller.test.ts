cdf69696447223a05be8e7fe3ad840bc5dfe5fb6
Victor Castro <victorcaucastro@gmail.com>
2026-01-26 23:52:12 -0300

    Removing test user

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,17 +1,26 @@
 import request from "supertest" // importação do "supertest" para fazer os testes.
+import { prisma } from "@/database/prisma"  // importação do prisma para conexão com o banco de dados.
 
 import { app } from "@/app"  // importação do onde estão as rotas.
 
 describe("UsersController", () => {  // teste para criação de novos usuários.
-  it("should create a new user sucessfully", async () => {
+  let user_id: string  // variável para pegar o id do usuário criado.  
+
+  afterAll(async () => {  
+    await prisma.user.delete({ where: { id: user_id } })  // método para deleter o usuário criado pelo teste após fazer o teste.
+  })
+
+  it("should create a new user sucessfully", async () => {   // teste de criação de um novo usuário.
     const response = await request(app).post("/users").send({
       name: "Test User",
-      email: "testuser@email.com",
+      email: "testuser@email.com",                 // campos necessários para a criação de um usuário.
       password: "password123",
     })
 
     expect(response.status).toBe(201)
-    expect(response.body).toHaveProperty("id")
+    expect(response.body).toHaveProperty("id")              // expectativa do que dever ser retornado ao fazer os testes.
     expect(response.body.name).toBe("Test User")
+
+    user_id = response.body.id  // aqui pego o id do "user" criado e passo ele para a variável no começo do método.
   })
 })
