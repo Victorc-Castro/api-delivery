80b7322d6339ee0d0ab6553506704cf4603fda31
Victor Castro <victorcaucastro@gmail.com>
2026-01-26 23:37:04 -0300

    Test in user registration

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,5 +1,17 @@
-describe("UsersController", () => {
-  it("test", () => {
-    console.log("Passou por aqui!")
+import request from "supertest" // importação do "supertest" para fazer os testes.
+
+import { app } from "@/app"  // importação do onde estão as rotas.
+
+describe("UsersController", () => {  // teste para criação de novos usuários.
+  it("should create a new user sucessfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@email.com",
+      password: "password123",
+    })
+
+    expect(response.status).toBe(201)
+    expect(response.body).toHaveProperty("id")
+    expect(response.body.name).toBe("Test User")
   })
 })
