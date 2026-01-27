7ef76fd5cb4d62da7ee6864a7e90742be9134535
Victor Castro <victorcaucastro@gmail.com>
2026-01-27 15:38:26 -0300

    Registration and validation test

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -2,6 +2,7 @@ import request from "supertest" // importação do "supertest" para fazer os tes
 import { prisma } from "@/database/prisma"  // importação do prisma para conexão com o banco de dados.
 
 import { app } from "@/app"  // importação do onde estão as rotas.
+import { email } from "zod/v4"
 
 describe("UsersController", () => {  // teste para criação de novos usuários.
   let user_id: string  // variável para pegar o id do usuário criado.  
@@ -22,5 +23,27 @@ describe("UsersController", () => {  // teste para criação de novos usuários.
     expect(response.body.name).toBe("Test User")
 
     user_id = response.body.id  // aqui pego o id do "user" criado e passo ele para a variável no começo do método.
+  })
+
+  it("should throw an error ir user with smae email already exists", async () => {  // teste de email já existente.
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email: "testuser@email.com",          // campos que serão criados para validar se determinado email já existe.   
+      password: "password123",
+    })
+
+    expect(response.status).toBe(400)       // expectativa do que dever ser retornado ao fazer os testes.
+    expect(response.body.message).toBe("User with same email already exists")
+  })
+
+  it("should throw a validation error if email is invalid", async () => {  // método para testar se o email é válido para cadastro.
+    const response = await request(app).post("/users").send({ 
+      name: "Test User",
+      email: "invalid-email",          // campos necessários para fazer o teste (campo e-mail é inválido para teste de exceção).
+      password: "password123"
+    })
+
+    expect(response.status).toBe(400)                           // expectativa do que dever ser retornado ao fazer os testes.        
+    expect(response.body.message).toBe("validation error")
   })
 })
